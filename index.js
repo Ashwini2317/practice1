8f9c98ce83568b200ae6af8e16570dde3f695875
ashwini <adpaithane23@gmail.com>
2025-06-21 12:50:21 +0530

    update

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
+require("path")
 
 const app = express()
+app.use(express.static("dist"))
 app.use(express.json())//body parser
-app.use(cors({ origin: "http://localhost:5173" }))
+app.use(cors({ origin: "https://practice1-vgd4.onrender.com" }))
 app.use("/api/employee", require("./routes/employee.routes"))
-mongoose.connect("mongodb://localhost:27017/employee")//database la connect karaychi command
+mongoose.connect(import.meta.env.VITE_URL)//database la connect karaychi command
 mongoose.connection.once("open", () => {
     console.log("db connected");
     app.listen(5000, console.log("server running"));// server open caraychi command nodemon takych
